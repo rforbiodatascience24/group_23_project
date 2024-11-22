63bf6ceb78f4b38a89f805a57462a0aee0233cb5
gaia-marraffa <s242160@dtu.dk>
2024-11-22 20:09:58 +0100

    tried to fix densitogram.R without success

--- R/densitogram.R
+++ R/densitogram.R
@@ -1,13 +1,13 @@
-densitogram <- function(genotype, protein) {
+densitogram <- function(genotype, protein, genotype_name) {
   dens_plot <- ggplot(data = genotype,
-         aes(x = protein,
-             fill = class)) +
+                      aes(x = protein,
+                          fill = class)) +
     geom_density(alpha = 0.5) +
     scale_fill_viridis_d(option = "D") +
     labs(title = paste0("Densitogram of ", 
                         protein, " for ", 
-                        deparse(substitute(genotype)), 
+                        genotype_name, 
                         " classes")) +
     theme_minimal(base_family = "Avenir")
-  return dens_plot
+  return(dens_plot)
 }
